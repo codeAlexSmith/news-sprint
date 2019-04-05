fd23ebcd199f3a026e31386a55423fba46cfbd64
Northcoders9 <alex-j.smith@arup.com>
2019-04-05 14:24:26 +0100

    make it better

--- db/data/index.js
+++ db/data/index.js
@@ -1,8 +1,10 @@
 const ENV = process.env.NODE_ENV || 'test'
 const development = require('./dev-data');
 const test = require('./test-data/index');
+const production = require('./test-data/index');
 
 const data = {
+    production,
   development,
   test,
 };
